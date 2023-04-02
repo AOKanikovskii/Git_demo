d547a600a3c5f99a9eb32e24032c7b22909cba28
AOKanikovskii <VrashFor@Gmail.com>
2023-04-02 03:42:29 +0200

    added sleep timer 5 sec for web page to be opened

--- Files/test_1.py
+++ Files/test_1.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 import pytest
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
@@ -7,5 +8,6 @@ browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 def test_open():
     browser.get('https://suninjuly.github.io/xpath_examples')
+time.sleep(5)
 
 
