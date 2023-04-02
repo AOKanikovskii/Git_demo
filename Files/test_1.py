9f5e402eef2f8aa9c9912813dc349d3cf0170850
AOKanikovskii <VrashFor@Gmail.com>
2023-04-02 04:08:56 +0200

    added test_open_page

--- Files/test_1.py
+++ Files/test_1.py
@@ -6,8 +6,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-def test_open():
-    browser.get('https://suninjuly.github.io/xpath_examples')
-time.sleep(5)
+# def test_open():
+#     browser.get('https://suninjuly.github.io/xpath_examples')
+#     browser.get('http://suninjuly.github.io/cats.html')
+#     browser.find_element("//*[contains(text(), 'Bullet cat')]")
+# time.sleep(5)
 
+def open_page():
+    browser.get('http://suninjuly.github.io/cats.html')    # открыли сайт
+    bullet_cat = browser.find_element(By.XPATH, "//*[contains(text(), 'Bullet cat')]")    # нашли элемент по XPATH
+    print(bullet_cat.text)
 
+    view_buttons = browser.find_elements(By.XPATH, "//*[contains(text(), 'View')]")
+    print(view_buttons)
+    assert len(view_buttons) == 6, 'Wrong length'
+
+def test_open_page():
+    open_page()
